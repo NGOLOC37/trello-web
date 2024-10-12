55de34270d7a743896dbbe0a323f982cfcaa4f29
NGOLOC37 <nguyenngoloc37.com>
2024-10-13 00:33:12 +0700

    code Board Bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,19 +1,96 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import Button from '@mui/material/Button'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+
+const MENU_STYLE = {
+  color: 'primary.main',
+  backgroundColor: 'white',
+  border: 'none',
+  px: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
     <Box
       sx={{
         width: '100%',
-        // height: (theme) => {
-        //   theme.trelloCustom.boardHeight
-        // },
         display: 'flex',
         alignItems: 'center',
-        height: (theme) => `${theme.trelloCustom.boardHeight}`
+        justifyContent: 'space-between',
+        height: (theme) => `${theme.trelloCustom.boardHeight}`,
+        gap: 2,
+        overflowX: 'auto',
+        borderTop: '1px solid #00bfa5'
       }}
     >
-      Board bar
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2
+        }}
+      >
+        <Chip
+          icon={<DashboardIcon />}
+          label="Ngo Loc 37"
+          clickable
+          sx={MENU_STYLE}
+        />
+        <Chip
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+          sx={MENU_STYLE}
+        />
+        <Chip
+          icon={<AddToDriveIcon />}
+          label="Add to Google Drive"
+          clickable
+          sx={MENU_STYLE}
+        />
+        <Chip
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+          sx={MENU_STYLE}
+        />
+        <Chip
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+          sx={MENU_STYLE}
+        />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2
+        }}
+      >
+        <Button variant="outlined">Invite</Button>
+        <AvatarGroup max={4}>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
